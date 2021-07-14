4946098d2aa3f15a81bace72790e3a8ba62a6782
Gustavo Jr <gustavinho-pzr@hotmail.com>
2021-07-14 17:17:48 -0300

    Projeto Keener.io

---
+++ Back/Estoque/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Dados do Estoque: " + p);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o número de produtos a ser adicionados:");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Estoque atualizado: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a serem removidos:");
+            int qtd = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Estoque atualizado: " + p);
+
+        }
+    }
+}
